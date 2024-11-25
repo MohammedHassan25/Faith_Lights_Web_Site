fbd03a22586eeafa8283ed9d34deedb47faa62c5
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2024-11-25 22:18:24 +0200

    circles for azkar sections

--- app/components/main/azkar.jsx
+++ app/components/main/azkar.jsx
@@ -2,13 +2,15 @@ import "./azkar.css"
 
 export default function Azkar() {
   return (
-    <div className="flex justify-center items-center shadow-inner shadow-neutral-300 h-screen bg-slate-900">
+    <div className="flex justify-center items-center shadow-inner shadow-neutral-300 h-screen bg-slate-900 relative overflow-hidden">
+      <img className="absolute circle-top" src="./img/circle.png" alt="circle"/>
       <div className="flex justify-between items-center flex-wrap w-full text-neutral-200 text-shadow-white main">
         <h1 className="text-8xl font-bold">الأذكار</h1>
         <p className="text-4xl para">متوفر الآن بعض الأدعية والأذكار المهمة مثل أذكار الصباح والمساء وأذكار دخول المسجد وغيرها</p>
         <button class="bg-cyan-500 shadow-lg shadow-cyan-500/50 mx-auto p-6 w-40 rounded-2xl text-2xl font-bold "><a href="/azkar">الأذكار</a></button>
       </div>
-      <img className="h-full w-full display" src="./img/azkar.png" alt="azkar" srcset="" />
+      <img className="h-full w-full display" src="./img/azkar.png" alt="azkar"/>
+      <img className="absolute circle-bottom" src="./img/circle.png" alt="circle"/>
     </div>
   )
 }
--- app/components/main/hadith.jsx
+++ app/components/main/hadith.jsx
@@ -3,7 +3,7 @@ import "./hadith.css";
 export default function Hadith() {
   return (
     <>
-      <div className="flex justify-center items-center w-11/12 m-auto p h-96 relative">
+      <div className="flex justify-center items-center w-11/12 m-auto p h-96 relative bg-white z-10">
         <img className="spin w-48" src="./img/islamic-circle.png" alt="islamic-circle" />
         <q className="m-0 font-bold hadith-content text-center line-height-5 sequence text-sky-400 shadow-slate-400 wrap-auto">
           عن جابر بن عبد الله رضي الله عنهما قال: سمعت رسول الله صلى الله عليه
