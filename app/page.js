5f2e10c8716fd1ed69fe2e977a0e636423547708
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2024-11-25 21:19:56 +0200

    Add Azkar section

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Azkar from "./components/main/azkar.jsx";
 import Hadith from "./components/main/hadith.jsx";
 import Landing from "./components/main/landing.jsx";
 
@@ -7,9 +8,7 @@ export default function Home() {
     <>
       <Landing />
       <Hadith />
-      <footer className="">
-
-      </footer>
+      <Azkar/>
     </>
   );
 }
