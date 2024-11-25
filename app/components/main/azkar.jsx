b164f4dcd6524097e9d0a970d209aa7e3e92ad90
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2024-11-25 21:26:15 +0200

    Update azkar.jsx

--- app/components/main/azkar.jsx
+++ app/components/main/azkar.jsx
@@ -6,7 +6,7 @@ export default function Azkar() {
       <div className="flex justify-between items-center flex-wrap w-full text-neutral-200 text-shadow-white main">
         <h1 className="text-8xl font-bold">الأذكار</h1>
         <p className="text-4xl para">متوفر الآن بعض الأدعية والأذكار المهمة مثل أذكار الصباح والمساء وأذكار دخول المسجد وغيرها</p>
-        <button class="bg-cyan-500 shadow-lg shadow-cyan-500/50 mx-auto p-6 w-40 rounded-2xl text-2xl font-bold ">الأذكار</button>
+        <button class="bg-cyan-500 shadow-lg shadow-cyan-500/50 mx-auto p-6 w-40 rounded-2xl text-2xl font-bold "><a href="/azkar">الأذكار</a></button>
       </div>
       <img className="h-full w-full display" src="./img/azkar.png" alt="azkar" srcset="" />
     </div>
