f579d259ac6f5c11c15f69f097525be0a9efc7ec
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2025-09-03 05:51:00 +0300

    fix error

--- app/components/SalatPage/SalatCard.jsx
+++ app/components/SalatPage/SalatCard.jsx
@@ -1,7 +1,7 @@
 export function SalatCard(props) {
   const { salat, time } = props;
   return (
-    <div className="flex-1 p-8 font-bold text-2xl text-center bg-blue-600 min-w-[300px] max-w-[300px] max-h-[300px] rounded-lg transition duration-300 ease-in-out hover:bg-blue-500 hover:scale-105">
+    <div className="flex-1 p-8 font-bold text-2xl text-center bg-blue-600 max-w-[300px] min-w-[300px] h-[192.29px] rounded-lg transition duration-300 ease-in-out hover:bg-blue-500 hover:scale-105">
       <h1 className="text-neutral-200 text-shadow-white">{salat}</h1>
       <p className="text-neutral-200 text-shadow-white">{time}</p>
     </div>
